8f6b35bad5dd9c16379ba8407d2d2ecacd8fc307
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-16 01:58:59 +0000

    :construction: create header component

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,39 @@
+'use-client'
+
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react"
+import { NavItem } from "./NavItem"
+
+export const Header = () => {
+    const [ navItens, _ ] = useState([
+        {
+            label: "Home",
+            href: "/"
+        },
+        {
+            label: "Projetos",
+            href: "/projects"
+        },
+    ])
+
+    return (
+        <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center">
+            <div className="container flex items-center justify-between">
+                <Link href="/">
+                    <Image
+                        width={58}
+                        height={49}
+                        src="/images/logo.svg"
+                        alt="Logo Fausantosdev"/>
+                </Link>
+
+                <nav className="flex items-center gap-4 sm:gap-10">
+                    {navItens.map((item, index) => (
+                        <NavItem {...item} key={index} />
+                    ))}
+                </nav>
+            </div>
+        </header>
+    )
+}
