56e85eb77005e973b3e0b641a319200dd7a3dd7a
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-16 23:04:40 -0300

    :construction: includes the HeroSection on the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import { HeroSection } from "./components/pages/home/HeroSection";
+
 export default async function Home() {
   return (
     <>
-      <h1>Hello! World.</h1>
+      <HeroSection />
     </>
   )
 }
