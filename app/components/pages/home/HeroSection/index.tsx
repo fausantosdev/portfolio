13c039d193eaf0b2758faf009626f37f5448792f
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-18 12:07:54 -0300

    :construction: remove font style

--- app/components/pages/home/HeroSection/index.tsx
+++ app/components/pages/home/HeroSection/index.tsx
@@ -26,7 +26,7 @@ const MOCK_CONTACTS = [
 
 export const HeroSection = () => {
     return (
-        <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:py-[110px] font-mono">
+        <section className="w-full lg:h-[755px] bg-hero-image bg-cover bg-center bg-no-repeat flex flex-col justify-end pb-10 sm:pb-32 py-32 lg:py-[110px]">
             <div className="container flex items-start justify-between flex-col-reverse lg:flex-row">
                 <div className="w-full lg:max-w-[530px]">
                     <p className="text-emerald-400">Olá, meu nome é</p>
