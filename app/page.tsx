c15c210a87579c746e6f01156662c9cae567957a
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-18 12:12:15 -0300

    :construction: create experience display component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HeroSection } from "./components/pages/home/HeroSection";
+import { KnownTechs } from "./components/pages/home/KnownTechs";
 
 export default async function Home() {
   return (
     <>
       <HeroSection />
+      <KnownTechs />
     </>
   )
 }
