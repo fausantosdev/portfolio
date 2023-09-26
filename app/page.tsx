5244536a80c2bad300ca2922f287682d531096db
Flávio dos Santos Marques <fausantosdev@hotmail.com>
2023-09-26 20:28:34 -0300

    :construction: adds professional experience component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { FeaturedProjects } from "./components/pages/home/FeaturedProjects";
 import { HeroSection } from "./components/pages/home/HeroSection";
 import { KnownTechs } from "./components/pages/home/KnownTechs";
+import { WorkExperience } from "./components/pages/home/WorkExperience";
 
 export default async function Home() {
   return (
@@ -8,6 +9,7 @@ export default async function Home() {
       <HeroSection />
       <KnownTechs />
       <FeaturedProjects />
+      <WorkExperience />
     </>
   )
 }
